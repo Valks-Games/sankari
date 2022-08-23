e5b66ae4edd3d40fa3277b3df4657210cbef2c11
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-22 19:08:04 -0500

    Platforms move back and fourth smoothly

--- Scripts/MovingPlatform.cs
+++ Scripts/MovingPlatform.cs
@@ -2,10 +2,23 @@ namespace Sankari;
 
 public class MovingPlatform : KinematicBody2D
 {
-    public override void _PhysicsProcess(float delta)
+    [Export] public int Duration = 3;
+    [Export] public int StartDelay = 0;
+    [Export] public Tween.TransitionType TransitionType = Tween.TransitionType.Cubic;
+    [Export] public Tween.EaseType EaseType = Tween.EaseType.InOut;
+
+    public override async void _Ready()
     {
-        var pos = Position;
-        pos.x += 0.1f;
-        Position = pos;
+        var tween = new GTween(this);
+
+        await tween.AnimatePlatform
+        (
+            Position, 
+            GetNode<Position2D>("Target").GlobalPosition, 
+            Duration, 
+            StartDelay, 
+            TransitionType, 
+            EaseType
+        );
     }
 }
--- Scripts/Utils/GTween.cs
+++ Scripts/Utils/GTween.cs
@@ -6,7 +6,8 @@ public class GTween
     private Node _target;
 
     /// <summary>
-    /// Should the tween animation loop?
+    /// Should the tween animation loop? Note that if Repeat is set to false than the values set in InterpolateProperty
+    /// will be wiped when the animation finishes.
     /// </summary>
     public bool Repeat
     {
@@ -30,12 +31,32 @@ public class GTween
         object initialValue, 
         object finalValue, 
         float duration, 
-        Tween.TransitionType transType = Tween.TransitionType.Linear, 
-        Tween.EaseType easeType = Tween.EaseType.InOut, 
-        float delay = 0
+        float delay = 0,
+        Tween.TransitionType transType = Tween.TransitionType.Cubic, 
+        Tween.EaseType easeType = Tween.EaseType.InOut
     ) => _tween.InterpolateProperty(_target, property, initialValue, finalValue, duration, transType, easeType, delay);
 
+    public async Task AnimatePlatform
+    (
+        object initialValue, 
+        object finalValue, 
+        float duration,
+        int startDelay,
+        Tween.TransitionType transType = Tween.TransitionType.Cubic,
+        Tween.EaseType easeType = Tween.EaseType.InOut
+    ) 
+    {
+        _tween.Repeat = true;
+        InterpolateProperty("position", initialValue, finalValue, duration, 0, transType, easeType);
+        InterpolateProperty("position", finalValue, initialValue, duration, duration, transType, easeType);
+        await Task.Delay(startDelay * 1000);
+        Start();
+    }
+
+    public void IsActive() => _tween.IsActive();
     public void Start() => _tween.Start();
+    public void Pause() => _tween.StopAll();
+    public void Resume() => _tween.ResumeAll();
 
     /// <summary>
     /// The name of the method passed must have Object @object, NodePath nodePath params
