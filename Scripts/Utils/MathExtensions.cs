1a1a456a132b203b4700e510f4d20d99def02504
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-27 15:59:45 -0500

    Convert pulse function to a extension in math

--- Scripts/Utils/MathExtensions.cs
+++ Scripts/Utils/MathExtensions.cs
@@ -14,4 +14,9 @@ public static class MathExtensions
     public static int Clamp(this int v, int min, int max) => Mathf.Clamp(v, min, max);
     public static float Clamp(this float v, float min, float max) => Mathf.Clamp(v, min, max);
     public static float Lerp(this float a, float b, float t) => Mathf.Lerp(a, b, t);
+
+    /// <summary>
+    /// Pulses a value from 0 to 1 to 0 to 1 over time
+    /// </summary>
+    public static float Pulse(this float time, float frequency) => 0.5f * (1 + Mathf.Sin(2 * Mathf.Pi * frequency * time));
 }
