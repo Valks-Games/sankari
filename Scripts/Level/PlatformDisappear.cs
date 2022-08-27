b53f0ed5798992283812ed721e9942ef2f150fbe
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-27 15:59:36 -0500

    Pulse logic for platform disappear

--- Scripts/Level/PlatformDisappear.cs
+++ Scripts/Level/PlatformDisappear.cs
@@ -3,12 +3,20 @@ namespace Sankari;
 public class PlatformDisappear : APlatform
 {
     private Sprite _sprite;
+    private ShaderMaterial _shaderMaterial;
+    private float _time;
 
     public override void _Ready()
     {
         Init();
 
         _sprite = GetNode<Sprite>("Sprite");
-        (_sprite.Material as ShaderMaterial).SetShaderParam("white_progress", 1);
+        _shaderMaterial = (_sprite.Material as ShaderMaterial);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        _time += delta;
+        _shaderMaterial.SetShaderParam("white_progress", _time.Pulse(2));
     }
 }
