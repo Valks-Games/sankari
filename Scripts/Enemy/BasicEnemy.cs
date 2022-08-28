66c1e065b3db31f1635e3135b6f8b11cf9877198
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-28 15:56:34 -0500

    animate basic enemy

--- Scripts/Enemy/BasicEnemy.cs
+++ Scripts/Enemy/BasicEnemy.cs
@@ -4,10 +4,17 @@ public class BasicEnemy : KinematicBody2D, IEnemy
 {
     private const float _gravity = 6000f;
     private bool _movingForward;
+    private AnimatedSprite _animatedSprite;
 
     public void PreInit(Player player)
     {
 
+    }
+
+    public override void _Ready()
+    {
+        _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        _animatedSprite.Play();
     }
 
     public override void _PhysicsProcess(float delta)
@@ -15,8 +22,11 @@ public class BasicEnemy : KinematicBody2D, IEnemy
         var velocity = new Vector2(0, 0);
         velocity.y += delta * _gravity;
 
-        if (IsOnWall())
+        if (IsOnWall()) 
+        {
             _movingForward = !_movingForward;
+            _animatedSprite.FlipH = true;
+        }
 
         if (_movingForward)
             velocity.x += 10;
