f38ced0291426786cc2cae4b5fb7e62879125b81
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-29 18:48:28 -0500

    Fix #1

---
+++ Scripts/Level/Bottom.cs
@@ -0,0 +1,14 @@
+namespace Sankari;
+
+public class Bottom : Node
+{
+    private void _on_Bottom_area_entered(Area2D area) 
+    {
+        var parent = area.GetParent();
+
+        if (parent is IEnemy) 
+        {
+            parent.QueueFree();
+        }
+    }
+}
