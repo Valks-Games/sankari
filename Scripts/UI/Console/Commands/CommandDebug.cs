1a8ed97de5bfaf7546a4a51dbac58f7f61691d9a
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-12 23:53:30 -0500

    Cleanup CommandDebug.cs

--- Scripts/UI/Console/Commands/CommandDebug.cs
+++ Scripts/UI/Console/Commands/CommandDebug.cs
@@ -5,30 +5,51 @@ namespace Sankari;
 
 public class CommandDebug : Command
 {
-    public CommandDebug() => Aliases = new[] { "x" };
+    private readonly Dictionary<string, Action> commands = new();
 
-    public override void Run(string[] args)
+    public CommandDebug() 
     {
-        if (args.Length == 0) 
-        {
-            Logger.Log("Please specify an argument");
-            return;
-        }
+        Aliases = new[] { "x" };
 
-        if (args[0] == "peerid") 
+        commands["help"] = () => 
         {
+            Logger.Log("List of commands:");
+            
+            foreach (var cmd in commands.Keys)
+                Logger.Log(cmd);
+        };
+
+        commands["peerid"] = () => 
+        {
+            if (!GameManager.Net.Client.IsRunning)
+            {
+                Logger.Log("Client is not running!");
+                return;
+            }
+
             Logger.Log(GameManager.Net.Client.PeerId);
-        }
+        };
 
-        if (args[0] == "players")
+        commands["players"] = () => 
         {
-            if (!GameManager.Net.Server.IsRunning) 
+            if (!GameManager.Net.Server.IsRunning)
             {
                 Logger.Log("Server is not running on this client");
                 return;
             }
 
             Logger.Log(GameManager.Net.Server.Players.PrintFull());
+        };
+    }
+
+    public override void Run(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Logger.Log("Usage: debug [cmd] (use 'debug help' for a list of commands");
+            return;
         }
+
+        commands[args[0]]();
     }
 }
