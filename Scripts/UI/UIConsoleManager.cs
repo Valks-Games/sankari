d51d3b6351d89c12e9f2de0bc54ac1e5f4677a14
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-13 21:05:23 -0500

    Fix #109

--- Scripts/UI/UIConsoleManager.cs
+++ Scripts/UI/UIConsoleManager.cs
@@ -2,10 +2,14 @@ namespace Sankari;
 
 public class UIConsoleManager : Control
 {
-    public bool ScrollToBottom { get; set; }
+    public bool ScrollToBottom { get; set; } = true;
 
     private TextEdit ConsoleLogs;
     private LineEdit ConsoleInput;
+
+    private readonly Dictionary<int, string> commandHistory = new();
+    private int commandHistoryIndex;
+    private int commandHistoryNav;
 
     public override void _Ready()
     {
@@ -35,6 +39,40 @@ public class UIConsoleManager : Control
             ConsoleLogs.ScrollVertical = Mathf.Inf;
     }
 
+    public override void _Input(InputEvent @event)
+    {
+        if (!Visible || commandHistory.Count == 0)
+            return;
+
+        if (Input.IsActionJustPressed("ui_up")) 
+        {
+            commandHistoryNav--;
+
+            if (!commandHistory.ContainsKey(commandHistoryNav)) 
+                commandHistoryNav++;
+
+            ConsoleInput.Text = commandHistory[commandHistoryNav];
+
+            // if deferred is not use then something else will override these settings
+            ConsoleInput.CallDeferred("grab_focus");
+            ConsoleInput.CallDeferred("set", "caret_position", commandHistory[commandHistoryNav].Length);
+        }
+
+        if (Input.IsActionJustPressed("ui_down"))
+        {
+            commandHistoryNav++;
+
+            if (!commandHistory.ContainsKey(commandHistoryNav)) 
+                commandHistoryNav--;
+
+            ConsoleInput.Text = commandHistory[commandHistoryNav];
+
+            // if deferred is not use then something else will override these settings
+            ConsoleInput.CallDeferred("grab_focus");
+            ConsoleInput.CallDeferred("set", "caret_position", commandHistory[commandHistoryNav].Length);
+        }
+    }
+
     private void _on_Console_Input_text_entered(string text)
     {
         var inputArr = text.Trim().ToLower().Split(' ');
@@ -45,8 +83,14 @@ public class UIConsoleManager : Control
 
         var command = Command.Instances.FirstOrDefault(x => x.IsMatch(cmd));
 
-        if (command != null)
-            command.Run(inputArr.Skip(1).ToArray());
+        if (command != null) 
+        {
+            var cmdArgs = inputArr.Skip(1).ToArray();
+
+            command.Run(cmdArgs);
+            commandHistory.Add(commandHistoryIndex++, $"{cmd}{(cmdArgs.Length == 0 ? "" : " ")}{string.Join(" ", cmdArgs)}");
+            commandHistoryNav = commandHistoryIndex;
+        }
         else
             Logger.Log($"The command '{cmd}' does not exist");
 
