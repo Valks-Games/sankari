64f9748b6a6e7f939139d450eb4a4b374a9cf93c
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-17 13:08:24 -0500

    Fix #120

--- Scripts/Managers/TransitionManager.cs
+++ Scripts/Managers/TransitionManager.cs
@@ -2,16 +2,9 @@ namespace Sankari;
 
 public partial class TransitionManager : Node
 {
-    private Tween tween;
-
-    public override void _Ready()
-    {
-        tween = GetTree().CreateTween();
-        tween.Stop();
-    }
-
     public async Task AlphaToBlack(float duration = 1.5f)
     {
+        var tween = GetTree().CreateTween();
         tween.TweenProperty(this, "color", new Color(0, 0, 0, 1), duration);
         tween.Play();
         await Task.Delay((int)duration * 1000);
@@ -19,6 +12,7 @@ public partial class TransitionManager : Node
 
     public void BlackToAlpha(float duration = 1.5f)
     {
+        var tween = GetTree().CreateTween();
         tween.TweenProperty(this, "color", new Color(0, 0, 0, 0), duration);
         tween.Play();
     }
