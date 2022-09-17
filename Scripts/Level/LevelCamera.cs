b8f87e0938808bfdbcd7db841a061282e91c6cef
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-17 17:46:03 -0500

    solve camera not following player

--- Scripts/Level/LevelCamera.cs
+++ Scripts/Level/LevelCamera.cs
@@ -7,13 +7,11 @@ public partial class LevelCamera : Camera2D
     public override void _Ready()
     {
         player = GetNode<Player>("../Player");
+		Current = true;
     }
 
-    public override void _Process(double delta)
-    {
-        Position = Player.Instance.Position;
-    }
+    public override void _Process(double delta) => Position = player.Position;
 
-    public void StopFollowingPlayer() => SetProcess(false);
+	public void StopFollowingPlayer() => SetProcess(false);
     public void StartFollowingPlayer() => SetProcess(true);
 }
