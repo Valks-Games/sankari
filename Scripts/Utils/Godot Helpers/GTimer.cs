4b73d465dbfc2ed28389ecf410b84121399d2149
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-26 13:50:48 -0500

    Fix GTimer not calling callback on Stop()

--- Scripts/Utils/Godot Helpers/GTimer.cs
+++ Scripts/Utils/Godot Helpers/GTimer.cs
@@ -5,16 +5,18 @@ namespace Sankari;
 public class GTimer
 {
     private Timer Timer { get; } = new();
+	private Callable Callable { get; }
 
-    public double TimeLeft 
-    { 
-        get { return Timer.TimeLeft; } 
+    public double TimeLeft
+    {
+        get { return Timer.TimeLeft; }
     }
 
     public GTimer(Node target, string methodName, int delayMs = 1000, bool loop = true, bool autoStart = true)
     {
         Init(target, delayMs, loop, autoStart);
-        Timer.Connect("timeout",new Callable(target,methodName));
+		Callable = new Callable(target, methodName);
+        Timer.Connect("timeout", Callable);
     }
 
     private void Init(Node target, int delayMs, bool loop, bool autoStart)
@@ -41,6 +43,10 @@ public class GTimer
     }
 
     public void Start() => Timer.Start();
-    public void Stop() => Timer.Stop();
+    public void Stop()
+	{
+		Timer.Stop();
+		Callable.Call();
+	}
     public void QueueFree() => Timer.QueueFree();
 }
