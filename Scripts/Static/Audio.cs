909004ac68cc9e74e2fce21b0d42e40be8bb584b
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-29 18:51:42 -0500

    Move where LoadSFX and LoadMusic are located

--- Scripts/Static/Audio.cs
+++ Scripts/Static/Audio.cs
@@ -15,30 +15,6 @@ public static class Audio
         MusicPlayer = musicPlayer;
         MusicPlayer.Volume = 100;
         SfxPlayer.Volume = 80;
-
-        LoadSoundEffects();
-        LoadSoundTracks();
-    }
-
-    private static void LoadSoundEffects()
-    {
-        LoadSFX("player_jump", "Movement/Jump/sfx_movement_jump1.wav");
-        LoadSFX("coin_pickup_1", "Environment/Coin Pickup/1/sfx_coin_single1.wav");
-        LoadSFX("coin_pickup_2", "Environment/Coin Pickup/2/coin.wav");
-        LoadSFX("dash", "Movement/Dash/swish-9.wav");
-
-        LoadSFX("game_over_1", "Game Over/1/retro-game-over.wav");
-        LoadSFX("game_over_2", "Game Over/2/game-over-dark-orchestra.wav");
-        LoadSFX("game_over_3", "Game Over/3/musical-game-over.wav");
-        LoadSFX("game_over_4", "Game Over/4/orchestra-game-over.wav");
-    }
-
-    private static void LoadSoundTracks()
-    {
-        LoadMusic("map_grassy", "Map/8bit Bossa/8bit Bossa.mp3");
-        LoadMusic("grassy_1", "Level/Grassy Peaceful/Chiptune Adventures/Juhani Junkala [Chiptune Adventures] 1. Stage 1.ogg");
-        LoadMusic("grassy_2", "Level/Grassy Peaceful/Chiptune Adventures/Juhani Junkala [Chiptune Adventures] 2. Stage 2.ogg");
-        LoadMusic("ice_1", "Level/Ice/Icy_Expanse.mp3");
     }
 
     public static void PlaySFX(string name, int volume = 100)
@@ -87,6 +63,6 @@ public static class Audio
     /// </summary>
     public static void SetSFXVolume(int v) => SfxPlayer.Volume = v;
 
-    private static void LoadSFX(string name, string path) => Sfx[name] = ResourceLoader.Load<AudioStream>($"res://Audio/SFX/{path}");
-    private static void LoadMusic(string name, string path) => Music[name] = ResourceLoader.Load<AudioStream>($"res://Audio/Music/{path}");
+    public static void LoadSFX(string name, string path) => Sfx[name] = ResourceLoader.Load<AudioStream>($"res://Audio/SFX/{path}");
+    public static void LoadMusic(string name, string path) => Music[name] = ResourceLoader.Load<AudioStream>($"res://Audio/Music/{path}");
 }
