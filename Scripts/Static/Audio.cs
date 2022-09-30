148fe9b6b7caccc525419d9037797de093b9fff8
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-29 19:02:41 -0500

    Add comments to Audio.cs

--- Scripts/Static/Audio.cs
+++ Scripts/Static/Audio.cs
@@ -17,6 +17,9 @@ public static class Audio
         SfxPlayer.Volume = 80;
     }
 
+	/// <summary>
+	/// Play a sound (volume ranges from 0 to 100)
+	/// </summary>
     public static void PlaySFX(string name, int volume = 100)
     {
         SfxPlayer.Volume = volume;
@@ -38,6 +41,9 @@ public static class Audio
         SfxPlayer.Play();
     }
 
+	/// <summary>
+	/// Play a music track (pitch values between 0.1 and 1 are slower, values higher than 1.0 are faster)
+	/// </summary>
     public static void PlayMusic(string name, float pitch = 1)
     {
         if (string.IsNullOrWhiteSpace(name))
@@ -56,13 +62,23 @@ public static class Audio
         MusicPlayer.Play();
     }
 
+	/// <summary>
+	/// Stop the music track currently being played
+	/// </summary>
     public static void StopMusic() => MusicPlayer.Stop();
 
     /// <summary>
-    /// Values range from 0 to 100
+    /// Set the SFX volume (values range from 0 to 100)
     /// </summary>
     public static void SetSFXVolume(int v) => SfxPlayer.Volume = v;
 
+	/// <summary>
+	/// Load a sound effect, for e.g. LoadSFX("player_jump", "Movement/Jump/sfx_movement_jump1.wav")
+	/// </summary>
     public static void LoadSFX(string name, string path) => Sfx[name] = ResourceLoader.Load<AudioStream>($"res://Audio/SFX/{path}");
-    public static void LoadMusic(string name, string path) => Music[name] = ResourceLoader.Load<AudioStream>($"res://Audio/Music/{path}");
+    
+	/// <summary>
+	/// Load a music track, for e.g. LoadMusic("ice_1", "Level/Ice/Icy_Expanse.mp3")
+	/// </summary>
+	public static void LoadMusic(string name, string path) => Music[name] = ResourceLoader.Load<AudioStream>($"res://Audio/Music/{path}");
 }
