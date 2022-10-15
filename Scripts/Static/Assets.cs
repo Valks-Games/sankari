85f0263248168e0df7cadf7c3bcc022d76d1da8c
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-10-15 02:24:13 -0500

    Assets.cs Formatting

--- Scripts/Static/Assets.cs
+++ Scripts/Static/Assets.cs
@@ -2,13 +2,13 @@ namespace Sankari;
 
 public static class Prefabs
 {
-    public static PackedScene Map { get; }             = LoadPrefab("Map");
+    public static PackedScene Map             { get; } = LoadPrefab("Map");
     public static PackedScene PlayerDashTrace { get; } = LoadPrefab("PlayerDashTrace");
-    public static PackedScene BasicEnemy { get; }      = LoadPrefab("Enemies/BasicEnemy");
-    public static PackedScene PopupMessage { get; }    = LoadPrefab("UI/Popups/PopupMessage");
-    public static PackedScene PopupError { get; }      = LoadPrefab("UI/Popups/PopupError");
-    public static PackedScene PopupLineEdit { get; }   = LoadPrefab("UI/Popups/PopupLineEdit");
-    public static PackedScene OtherPlayer { get; }     = LoadPrefab("OtherPlayer");
+    public static PackedScene BasicEnemy      { get; } = LoadPrefab("Enemies/BasicEnemy");
+    public static PackedScene PopupMessage    { get; } = LoadPrefab("UI/Popups/PopupMessage");
+    public static PackedScene PopupError      { get; } = LoadPrefab("UI/Popups/PopupError");
+    public static PackedScene PopupLineEdit   { get; } = LoadPrefab("UI/Popups/PopupLineEdit");
+    public static PackedScene OtherPlayer     { get; } = LoadPrefab("OtherPlayer");
 
     private static PackedScene LoadPrefab(string path) => ResourceLoader.Load<PackedScene>($"res://Scenes/Prefabs/{path}.tscn");
 }
