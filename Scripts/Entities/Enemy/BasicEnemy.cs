28c2934a429e57363ec81574fabfb9c32a42e412
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-10-31 14:36:05 -0500

    Apply raycast abstraction to enemy

--- Scripts/Entities/Enemy/BasicEnemy.cs
+++ Scripts/Entities/Enemy/BasicEnemy.cs
@@ -5,44 +5,17 @@ public partial class BasicEnemy : Entity, IEnemy, IEntity, IEntityMovement
 	[Export] public float Speed { get; set; } = 40;
 	[Export] public bool Active { get; set; } = true;
 	[Export] public bool StartWalkingRight { get; set; }
-	[Export] public bool DontCollideWithWall { get; set; }
-	[Export] public bool FallOffCliff { get; set; }
 
 	public override int Gravity { get; set; } = 30000;
 	public AnimatedSprite2D AnimatedSprite { get; set; }
 	private bool MovingForward { get; set; }
 
-	private RayCast2D RayCastWallLeft { get; set; }
-	private RayCast2D RayCastWallRight { get; set; }
-	private RayCast2D RayCastCliffLeft { get; set; }
-	private RayCast2D RayCastCliffRight { get; set; }
-	private RayCast2D RayCastGroundMiddle { get; set; }
 	public Window Tree { get; set; }
 
 	public override void Init()
 	{
 		AnimatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		Activate();
-
-		RayCastWallLeft = GetNode<RayCast2D>("Wall Checks/Left");
-		RayCastWallRight = GetNode<RayCast2D>("Wall Checks/Right");
-		RayCastCliffLeft = GetNode<RayCast2D>("Cliff Checks/Left");
-		RayCastCliffRight = GetNode<RayCast2D>("Cliff Checks/Right");
-		RayCastGroundMiddle = GetNode<RayCast2D>("Ground Checks/Middle");
-
-		RayCast2DGroundChecks.Add(RayCastGroundMiddle);
-
-		if (FallOffCliff)
-		{
-			RayCastCliffLeft.Enabled = false;
-			RayCastCliffRight.Enabled = false;
-		}
-
-		if (DontCollideWithWall)
-		{
-			RayCastWallLeft.Enabled = false;
-			RayCastWallRight.Enabled = false;
-		}
 
 		if (StartWalkingRight)
 		{
@@ -72,13 +45,13 @@ public partial class BasicEnemy : Entity, IEnemy, IEntity, IEntityMovement
 
 			// If the entity is set to collide with a wall then change directions
 			// when touching a wall
-			if (!DontCollideWithWall && IsRaycastColliding(RayCastWallRight))
+			if (!DontCollideWithWall && IsNearWallRight())
 				ChangeDirection();
 
 			// If the entity is set to not fall off a cliff and there is no ground
 			// to the right in front of the entity then assume there is a cliff and
 			// prevent the entity from falling off the cliff by changing directions
-			if (!FallOffCliff && !IsRaycastColliding(RayCastCliffRight))
+			if (!FallOffCliff && !IsNearCliffRight())
 				ChangeDirection();
 		}
 		else
@@ -87,13 +60,13 @@ public partial class BasicEnemy : Entity, IEnemy, IEntity, IEntityMovement
 
 			// If the entity is set to collide with a wall then change directions
 			// when touching a wall
-			if (!DontCollideWithWall && IsRaycastColliding(RayCastWallLeft))
+			if (!DontCollideWithWall && IsNearWallLeft())
 				ChangeDirection();
 
 			// If the entity is set to not fall off a cliff and there is no ground
 			// to the left in front of the entity then assume there is a cliff and
 			// prevent the entity from falling off the cliff by changing directions
-			if (!FallOffCliff && !IsRaycastColliding(RayCastCliffLeft))
+			if (!FallOffCliff && !IsNearCliffLeft())
 				ChangeDirection();
 		}
 	}
@@ -121,18 +94,6 @@ public partial class BasicEnemy : Entity, IEnemy, IEntity, IEntityMovement
 	{
 		MovingForward = !MovingForward;
 		AnimatedSprite.FlipH = !AnimatedSprite.FlipH;
-	}
-
-	private bool IsRaycastColliding(RayCast2D raycast)
-	{
-		var collider = raycast.GetCollider() as Node;
-
-		if (collider != null && collider.IsInGroup("Tileset"))
-		{
-			return true;
-		}
-
-		return false;
 	}
 
 	private void _on_enemy_area_entered(Area2D area)
