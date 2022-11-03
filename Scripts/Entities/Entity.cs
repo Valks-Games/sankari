e30bd374e25e61e163fb5f9dacdd2ad2023316da
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-11-03 02:06:11 -0500

    Add the start of static entity class

---
+++ Scripts/Entities/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace Sankari;
+
+public abstract partial class Entity : StaticBody2D
+{
+	public float Delta { get; set; }
+
+	public sealed override void _Ready()
+	{
+		Init();
+	}
+
+	public sealed override void _PhysicsProcess(double delta)
+	{
+		Delta = (float)delta;
+		UpdatePhysics();
+	}
+
+	public abstract void Init();
+
+	public abstract void UpdatePhysics();
+}
