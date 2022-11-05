81e67e4f034c9ec556022a575dc1767d55569fe6
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-11-05 12:21:00 -0500

    Implement Trapper type enemy

---
+++ Scripts/Entities/Enemy/Trapper.cs
@@ -0,0 +1,50 @@
+namespace Sankari;
+
+public partial class Trapper : Entity
+{
+	private Area2D AreaDamage { get; set; }
+	private GTimer TimerReveal { get; set; }
+	private GTimer TimerRevealCooldown { get; set; }
+	private bool Revealed { get; set; }
+
+	public override void Init()
+	{
+		AreaDamage = GetNode<Area2D>("Damage");
+		TimerReveal = new GTimer(this, nameof(OnTimerReveal), 1500, false) { Loop = false };
+		TimerRevealCooldown = new GTimer(this, 2000, false) { Loop = false };
+		AnimatedSprite.Play("idle");
+	}
+
+	public override void UpdatePhysics()
+	{
+		
+	}
+
+	private void _on_damage_area_entered(Area2D area)
+	{
+		if (area.GetParent() is Player player)
+			player.RemoveHealth(1);
+	}
+
+	private void _on_detection_area_entered(Area2D area)
+	{
+		if (Revealed || TimerRevealCooldown.IsActive())
+			return;
+
+		if (area.GetParent() is Player)
+		{
+			Revealed = true;
+			AnimatedSprite.Play("reveal");
+			AreaDamage.SetDeferred("monitoring", true);
+			TimerReveal.Start();
+		}
+	}
+
+	private void OnTimerReveal()
+	{
+		Revealed = false;
+		AreaDamage.SetDeferred("monitoring", false);
+		AnimatedSprite.Play("hide");
+		TimerRevealCooldown.Start();
+	}
+}
--- Scripts/Entities/Entity.cs
+++ Scripts/Entities/Entity.cs
@@ -3,9 +3,11 @@
 public abstract partial class Entity : StaticBody2D
 {
 	public float Delta { get; set; }
+	public AnimatedSprite2D AnimatedSprite { get; set; }
 
 	public sealed override void _Ready()
 	{
+		AnimatedSprite = GetNodeOrNull<AnimatedSprite2D>("AnimatedSprite2D");
 		Init();
 	}
 
