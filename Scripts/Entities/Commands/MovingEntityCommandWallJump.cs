78b7a93d30a19e416ae7dee88382807405e27f5c
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-11-08 01:40:57 -0600

    Major refactor on player wall jumping script

--- Scripts/Entities/Commands/MovingEntityCommandWallJump.cs
+++ Scripts/Entities/Commands/MovingEntityCommandWallJump.cs
@@ -2,123 +2,120 @@
 
 public class MovingEntityCommandWallJump : EntityCommand<MovingEntity>
 {
+	// constants
 	public int JumpForceWallHorz { get; set; } = 800; // Horizontal wall jump force
 	public int JumpForceWallVert { get; set; } = 500; // Vertical wall jump force
 	public int MaxGravitySpeedSliding { get; set; } = 20; // Sliding force downwards
 	public int MaxGravitySpeedSlidingFast { get; set; } = 220; // Fast sliding force downwards
+	private float JumpForceMultiplierVert { get; set; } = 0.95f;
+	private float JumpForceMultiplierHorz { get; set; } = 0.5f;
+	private int JumpsOnSameWallMax { get; set; } = 2;
 
+	// variables
+	private int PreviousWall { get; set; }
+	private int WallDir { get; set; }
+	private int JumpsOnSameWall { get; set; }
+
+	// events
 	public event EventHandler WallJump;
-	private int previousWallOnJump;
-	private bool wasSliding = false;
-	private float previousXDir;
-	private int wallDir;
 
 	public MovingEntityCommandWallJump(MovingEntity entity) : base(entity) { }
 
 	public override void Start()
 	{
-		if (Entity.InWallJumpArea)
+		if (!Entity.InWallJumpArea)
 		{
-			// If the entity is on a wall, prevent entity from wall jumping on the same wall twice
-			if (wallDir != 0)
-			{
-				// wall jump
-				WallJump?.Invoke(this, EventArgs.Empty);
+			Entity.AnimatedSprite.FlipH = false;
+			return;
+		}
 
-				Entity.AnimatedSprite.FlipH = wallDir == 1; // flip sprite on wall jump
+		if (!OnAWall())
+			return;
 
-				var velocity = Entity.Velocity;
-				velocity.x += -JumpForceWallHorz * wallDir;
-				velocity.y = -JumpForceWallVert;
-				if (previousWallOnJump == wallDir)
-				{
-					velocity.y /= 2;
-				}
-				Entity.Velocity = velocity;
+		Entity.AnimatedSprite.FlipH = WallDir == 1; // flip sprite on wall jump
 
-				previousWallOnJump = wallDir;
-			}
+		var velocity = Entity.Velocity;
+
+		if (JumpOnSameWall())
+		{
+			// Only jump on the same wall if wall jumps does not exceed maximum same wall jumps
+			if (++JumpsOnSameWall >= JumpsOnSameWallMax)
+				return;
+
+			WallJump?.Invoke(this, EventArgs.Empty);
+
+			// Decrease value to reduce the jump force multiplier further
+			// A value of 0.1 would be very dramatic while a value of 0.95
+			// would be hard to notice
+			var jumpForceReductionVert = 0.75f;
+			var jumpForceReductionHorz = 0.5f;
+
+			// Reduce wall jump force multiplier if jumping from the same wall
+			// For e.g. a jump force multiplier of 0.75 would become 0.60
+			// then 0.48 and so on
+			// If the player jumped on the same wall 5 times they would gain
+			// no more jump force
+			JumpForceMultiplierVert *= jumpForceReductionVert;
+			JumpForceMultiplierHorz *= jumpForceReductionHorz;
+
+			velocity.x += -JumpForceWallHorz * WallDir * JumpForceMultiplierHorz;
+			velocity.y = -JumpForceWallVert * JumpForceMultiplierVert;
 		}
 		else
-			Entity.AnimatedSprite.FlipH = false;
+		{
+			// Wall jump is on a different wall, reset jumps on same wall
+			JumpsOnSameWall = 0;
+
+			WallJump?.Invoke(this, EventArgs.Empty);
+
+			// Wall jump is on a diferent wall, reset the jump force multipliers to
+			// their default values
+			JumpForceMultiplierVert = 0.95f;
+			JumpForceMultiplierHorz = 0.5f;
+
+			velocity.x += -JumpForceWallHorz * WallDir;
+			velocity.y = -JumpForceWallVert;
+		}
+
+		Entity.Velocity = velocity;
+
+		PreviousWall = WallDir;
 	}
 
 	public override void Update(float delta)
 	{
-		wallDir = UpdateWallDirection();
+		WallDir = UpdateWallDirection();
+
 		if (Entity.IsNearGround())
 		{
-			previousWallOnJump = 0;
-			wasSliding = false;
+			PreviousWall = 0;
 			return;
 		}
 
-		var isSliding = IsSliding();
-
-		if (isSliding)
+		if (Entity.InWallJumpArea && OnAWall())
 		{
 			var velocity = Entity.Velocity;
 
 			if (Entity.IsFalling())
 			{
-				Entity.ModGravityMaxSpeed = MaxGravitySpeedSliding;
+				Entity.FakeGravitySpeed = MaxGravitySpeedSliding;
 
 				// fast fall
 				if (Entity is Player player)
 					if (player.PlayerInput.IsDown)
-						Entity.ModGravityMaxSpeed = MaxGravitySpeedSlidingFast;
+						Entity.FakeGravitySpeed = MaxGravitySpeedSlidingFast;
 
-				// Slow down the player. (Faking Friction)
-				if (velocity.y != Entity.ModGravityMaxSpeed)
-					velocity = velocity.MoveToward(new Vector2(velocity.x, Entity.ModGravityMaxSpeed), 2000 * delta);
+				// Slow down the player
+				//if (velocity.y != Entity.FakeGravitySpeed)
+					velocity = velocity.MoveToward(new Vector2(velocity.x, Entity.FakeGravitySpeed), 2000 * delta);
 			}
+
 			Entity.Velocity = velocity;
 		}
-
-		wasSliding = isSliding;
 	}
 
-	/// <summary>
-	/// Gets the raycast which is colliding
-	/// </summary>
-	private RayCast2D GetCollidingWall()
-	{
-		if (wallDir == Vector2.Left.x)
-			return Entity.RaycastsWallLeft.GetAnyRayCastCollider();
-
-		else if (wallDir == Vector2.Right.x)
-			return Entity.RaycastsWallRight.GetAnyRayCastCollider();
-
-		return default;
-	}
-
-	/// <summary>
-	/// Checks if the Entity should be sliding
-	/// </summary>
-	private bool IsSliding()
-	{
-		var velocityDir = MovementUtils.GetDirection(Entity.Velocity);
-
-		// MoveDir takes priority
-		if (Entity.MoveDir.x != 0)
-			previousXDir = Entity.MoveDir.x;
-		else if (velocityDir.x != 0)
-			previousXDir = velocityDir.x;
-
-		var isSliding = wasSliding;
-
-		if (Entity.InWallJumpArea && wallDir != 0)
-		{
-			if (previousXDir == wallDir)
-				isSliding = true;
-			else if (previousXDir != 0)
-				isSliding = false;
-		}
-		else
-			isSliding = false;
-
-		return isSliding;
-	}
+	private bool OnAWall() => WallDir != 0;
+	private bool JumpOnSameWall() => PreviousWall == WallDir;
 
 	private int UpdateWallDirection()
 	{
