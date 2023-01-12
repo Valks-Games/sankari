d2a87650e7e30f5a0b335978dc0d2e9a5094c6c0
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-01-11 19:20:27 -0600

    half the time dash trace is visible

--- Scripts/Entities/Player/PlayerDashTrace.cs
+++ Scripts/Entities/Player/PlayerDashTrace.cs
@@ -6,7 +6,7 @@ public partial class PlayerDashTrace : Sprite2D
 
     public override void _Ready()
     {
-        Timer = new GTimer(this, nameof(OnTimerDone), 200);
+        Timer = new GTimer(this, nameof(OnTimerDone), 100);
     }
 
     private void OnTimerDone()
