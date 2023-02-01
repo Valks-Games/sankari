515912bdcb8e675692e8b29415cbc869ae655a00
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-01-31 18:50:14 -0600

    simplify use of timer in dash trace

--- Scripts/Entities/Player/PlayerDashTrace.cs
+++ Scripts/Entities/Player/PlayerDashTrace.cs
@@ -2,15 +2,6 @@ namespace Sankari;
 
 public partial class PlayerDashTrace : Sprite2D
 {
-    private GTimer Timer { get; set; }
-
-    public override void _Ready()
-    {
-        Timer = new GTimer(this, nameof(OnTimerDone), 100);
-    }
-
-    private void OnTimerDone()
-    {
-        QueueFree();
-    }
+    public override void _Ready() =>
+		GetTree().CreateTimer(0.1).Timeout += () => QueueFree();
 }
